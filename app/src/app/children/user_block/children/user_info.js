b4d67aa9db82a12a8f0a2e57bc9da5499b0e42b6
Lucy Murray <lucyamurray@me.com>
2021-11-18 07:57:54 +0000

    last stying fix and alt tag

--- app/src/app/children/user_block/children/user_info.js
+++ app/src/app/children/user_block/children/user_info.js
@@ -4,7 +4,7 @@ import styles from "../styles";
 const UserInfo = ({ user, location }) => {
   return (
     <div style={styles.infoContainer}>
-      <img src={user.profile_pic_url} style={styles.image} />
+      <img src={user.profile_pic_url} alt={user.username} style={styles.image} />
       <div style={styles.infoText}>
         <p style={styles.username}>{user.username}</p>
         <p style={styles.location}>{location.name}</p>
--- app/src/app/children/user_block/styles.js
+++ app/src/app/children/user_block/styles.js
@@ -1,18 +1,21 @@
 export default {
     infoContainer: {
-        display: "flex",
-        padding: "20px 0",
-        margin: "0 20px",
-        borderBottom: "1px solid lightgray"
+        display: 'flex',
+        padding: '20px 0',
+        margin: '0 20px',
+        borderBottom: '1px solid lightgray'
     },
     infoText: {
-        marginLeft: "10px"
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        marginLeft: '10px'
     },
     image: {
-        height: "48px"
+        height: '48px'
     },
     username: {
-        fontWeight: "600",
+        fontWeight: '600',
         margin: 0
     },
     location: {
